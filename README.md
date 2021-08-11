3465e40866c1e65c5e1bfb1cfb8a6850311fa4db
Juhair Islam <juhair98@gmail.com>
2021-08-11 19:49:55 +0600

    added: mongoose

--- README.md
+++ README.md
@@ -4,10 +4,29 @@ This project is for learning Express and mongoDB. I will try to make a todoList 
 
 ## How to install!
 
+In this project I used Ubuntu 21.04 x86_64 node v12.21.0 MongoDB v4.4.8.
+
+1. Install the required application, nodejs, yarn, mongodb.
+1. Download this file and install using `yarn`.
+1. Run the application using `yarn start`.
+
 ## How to work With it!
+
+Use postman to play with the api.
+
+- Every tuple have 5 attribute.
+  - Task name (String)
+  - Done (Boolean)
+  - Starting time (Time)
+  - Ending time (Time)
+  - Status (String)
+- ...
 
 ## TO-DO:
 
-- [ ] Create a express base
-- [ ] Connect with MongoDB
-- [ ] ...
+- [x] Create a noob express base
+- [x] Connect with MongoDB
+- [ ] Use Create and show database
+- [ ] Take a input for database
+- [ ] Mark as task complete
+- [ ] Sorting...
--- main.js
+++ main.js
@@ -1,11 +1,29 @@
+//initialization
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const port = 6969;
+const port = process.env.PORT || 6969;
 
+//connect to database
+mongoose
+  .connect("mongodb://localhost/todo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((error) => handleError(error));
+const db = mongoose.connection;
+
+//Routes
 app.get("/", (req, res) => {
   res.send("ExpressCURD");
 });
 
-app.listen(port, () => {
-  console.log(`Catch me @ http://localhost:${port}`);
+//If database is connected
+//Then open server port
+db.once("open", function () {
+  app.listen(port, () => {
+    console.log(`Fire in the hole!!! @ http://localhost:${port}`);
+  });
 });
+//send Error msg for Database
+db.on("error", console.error.bind(console, "connection error:"));
