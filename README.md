36a2eb372f65f075012da5e27e5db608ceaff74d
Juhair Islam <juhair98@gmail.com>
2021-08-16 00:59:48 +0600

    added: basic CRUD

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# ExpressCURD
+# ExpressCRUD
 
-This project is for learning Express and mongoDB. I will try to make a todoList app without just using api without any UI in local host.
+This project is for learning Express and mongoDB. I will try to make a todo app just using api without any UI in local host.
 
 ## How to install!
 
@@ -15,18 +15,27 @@ In this project I used Ubuntu 21.04 x86_64 node v12.21.0 MongoDB v4.4.8.
 Use postman to play with the api.
 
 - Every tuple have 5 attribute.
-  - Task name (String)
-  - Done (Boolean)
-  - Starting time (Time)
-  - Ending time (Time)
-  - Status (String)
+  - title (String)
+  - done (Boolean)
+  - startDate (Date)
+  - EndDate time (Date)
+  - comment (String)
 - ...
 
 ## TO-DO:
 
-- [x] Create a noob express base
-- [x] Connect with MongoDB
-- [ ] Use Create and show database
-- [ ] Take a input for database
-- [ ] Mark as task complete
-- [ ] Sorting...
+- [x] Create a noob express base.
+- [x] Connect with MongoDB.
+- [x] Create the DB.
+- [x] Create a entry with or without comment.
+- [ ] Update a entry with title or end date or both.
+- [x] Remove a entry by id.
+- [ ] add comment.
+- [x] Mark as task complete or not completed( add extra time ).
+- [x] Show as done/ not done/ all data.
+- [ ] Show only one entry.
+- [ ] Show as end date shorted.
+- [ ] Show as end date (end in a week or end in a month or...).
+- [ ] Show due date for certain entry.
+- [ ] Show the time it took or will take.
+- [ ] Show overdue Task.
--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 6969;
 
+//read the json type input as post
+app.use(express.json());
+
+//...
+//Database
+//...
+
 //connect to database
 mongoose
   .connect("mongodb://localhost/todo", {
@@ -13,11 +20,210 @@ mongoose
   .catch((error) => handleError(error));
 const db = mongoose.connection;
 
-//Routes
-app.get("/", (req, res) => {
-  res.send("ExpressCURD");
+//Create a schema
+const Schema = mongoose.Schema;
+const todoSchema = new Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  done: {
+    type: Boolean,
+    default: false,
+  },
+  startDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  endDate: {
+    type: Date,
+    require: true,
+  },
+  comment: {
+    type: String,
+    default: "Hentai",
+  },
 });
 
+const DB = mongoose.model("todo", todoSchema);
+
+//...
+//Routes
+//...
+
+app.get("/", (req, res) => {
+  res.send("https://github.com/Error6251/ExpressCRUD");
+});
+
+//Show not completed result
+app.get("/show", (req, res) => {
+  DB.find({ done: false }).then((result) => {
+    res.send(result);
+  });
+});
+
+//show all results
+app.get("/show/all", (req, res) => {
+  DB.find().then((result) => {
+    res.send(result);
+  });
+});
+
+//show completed results
+app.get("/show/done", (req, res) => {
+  DB.find({ done: true }).then((result) => {
+    res.send(result);
+  });
+});
+
+//insert a value
+app.post("/new", (req, res) => {
+  //calculate the end date
+  let endDate = new Date();
+  endDate.setDate(endDate.getDate() + Math.abs(req.body.endIn));
+
+  //take the input
+  const inp = new DB({
+    title: req.body.title,
+    endDate: endDate,
+    comment:
+      req.body.comment || `This task will take ${Math.abs(req.body.endIn)} day`,
+  });
+
+  //save it on DB
+  inp
+    .save()
+    .then((data) => {
+      res.send({
+        message: "New Task added",
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Ohhh... something is wrong",
+      });
+    });
+});
+
+//update or edit a value
+app.put("/edit", (req, res) => {
+  //calculate the end date
+  let endDate = new Date();
+  endDate.setDate(endDate.getDate() + Math.abs(req.body.endIn));
+
+  // //check there is title or date
+  // if (req.body.title != null) {
+  //   title: req.body.title;
+  // }
+
+  //update It
+  DB.findByIdAndUpdate(
+    req.body.id,
+    {
+      title: req.body.title || "I forget the title",
+      endDate: endDate,
+    },
+    { useFindAndModify: false }
+  )
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: "Error with id " + req.body.id,
+        });
+      } else {
+        res.send({
+          message: "updated " + req.body.id,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error with id " + req.body.id,
+      });
+    });
+});
+
+//delete a value
+app.delete("/remove", (req, res) => {
+  const id = req.body.id;
+
+  DB.findByIdAndRemove(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Error on id=${id}.`,
+        });
+      } else {
+        res.send({
+          message: "That task is gone!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error on id=" + id,
+      });
+    });
+});
+
+//mark as done
+app.get("/done/:id", (req, res) => {
+  DB.findByIdAndUpdate(
+    req.params.id,
+    {
+      done: true,
+      endDate: Date.now(),
+    },
+    { useFindAndModify: false }
+  )
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: "Error with id " + req.params.id,
+        });
+      } else {
+        res.send({
+          message: "Super cO0oL, " + req.params.id + " is Done!!!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error with id " + req.params.id,
+      });
+    });
+});
+
+//mark as undone
+app.put("/undone/:id", (req, res) => {
+  DB.findByIdAndUpdate(
+    req.params.id,
+    {
+      done: false,
+    },
+    { useFindAndModify: false }
+  )
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: "Error with id " + req.params.id,
+        });
+      } else {
+        res.send({
+          message: "S()ooo SAaD, " + req.params.id + " is Undone",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error with id " + req.params.id,
+      });
+    });
+});
+
+//...
+//Turn on the system
+//...
 //If database is connected
 //Then open server port
 db.once("open", function () {
